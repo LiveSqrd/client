03b2c28d36e5c48d49615b01693bb2c104472ee1
Antonio pelle Pellegrino <pelle@circles.io>
2014-11-19 16:27:12 -0500

    Yodate

--- hooks/user.js
+++ hooks/user.js
@@ -1,6 +1,7 @@
 var  http 		= require('http')
 	, _ 	 	= require('underscore')
 	, hook 		= {}
+	, Q 		= require("q")
 	, db;
 
 exports.run = function(options){
@@ -10,39 +11,60 @@ exports.newUser = function(client,profile){
 
 }
 exports.login = function(session,callback){
-	var json = {};
 	var options = {}
+	,promiseRel = {}
+	,json = {}
+	,promises = [];
 	options.one = true;
 	options.select = {"login":0,"ip":0,"__v":0}
-	if(_.has(session,"auth")){
-		json.result = {}
-
+	//console.log("start with session: ",session)
+	if(session.i){
+		promiseRel.i = Q.defer();
+		promises.push(promiseRel.i.promise); 
 		db.read("instance",{"_id":session.i},options,function(e0,d0){
 
 			json.error 		= e0;
 			json.result.i 	= d0;
-			if(_.has(json.result,"c") && _.has(json.result,"p"))
-				callback(json);
+			promiseRel.i.resolve({key:"i",result:d0})
 		})
+	}
+	if(session.c){
+		promiseRel.c = Q.defer();
+		promises.push(promiseRel.c.promise); 
 		db.read("client",{"_id":session.c},options,function(e0,d0){
 
 			json.error 		= e0;
 			json.result.c 	= d0;
-			if(_.has(json.result,"i") && _.has(json.result,"p"))
-				callback(json);
+			promiseRel.c.resolve({key:"c",result:d0})
 		})
+	}
+	if(session.p) {
+		promiseRel.p = Q.defer();
+
+		promises.push(promiseRel.p.promise); 
 		db.read("profile",{"_id":session.p},options,function(e0,d0){
 
 			json.error 		= e0;
 			json.result.p 	= d0;
-			if(_.has(json.result,"i") && _.has(json.result,"c"))
-				callback(json);
+			promiseRel.p.resolve({key:"p",result:d0})
 		})
-
-	}else{
-		json.error = "Not Logged in";
-		callback(json);
 	}
+		json.result = {}
+		
+	if(!_.has(session,"auth"))
+		json.error = "Not Logged in";
+
+			
+	Q.allSettled(promises)
+	.then(function(results){
+		results.forEach(function (result) {
+			//console.log("done with",result.value.key)
+			json.result[result.value.key] = result.value.result;
+		})
+		callback(json)
+
+	})
+
 }
 exports.logout = function(session){
 }
